6acf02204532e0116172c57c6865686174403b13
Archit <dwevediar@gmail.com>
2020-05-17 17:12:08 +0530

    LeetCode Package Update

--- leetCode/__main__.py
+++ leetCode/__main__.py
@@ -1,9 +1,58 @@
 import sys, requests
 from leetCode.help import validHelp, invalidHelp
 
+# Web Scraping 
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+import urllib.request
+import time
+
+
 def main():
 	args = sys.argv[1:]
+	
+	# Set of commands for package
+	commands = {
+		"set": setupProblem, 
+		"check": checkProblem, 
+		"visualize": visualize,
+	    "customVisualize": customVisualize
+	}
+
+	# Condition to check if the commands
+	# are good enough to go with
 	if len(args):
-		pass
+		if args[0] in commands:
+			commands[args[0]](args[1:])
+		else:
+			invalidHelp()
 	else:
 		validHelp()
+
+
+def setupProblem(args):
+	link = args[0]
+	if link.startswith("https://leetcode.com/problems/"):
+		try:
+			response = requests.get(link)
+			pageHTML = response.content
+			page_soup = soup(pageHTML, "html.parser")
+			page_soup.prettify()
+			# containers=page_soup.findAll("pre")
+			print(page_soup)
+		except:
+			print("Invalid LeetCode Problem")
+	else:
+		print("Invalid LeetCode Problem") 
+
+def checkProblem(args):
+	print("Under Maintainance")
+
+
+
+def visualize(args):
+	print("Under Maintainance")
+
+
+def customVisualize(args):
+	print("Under Maintainance")
--- leetCode/help.py
+++ leetCode/help.py
@@ -1,25 +1,25 @@
 
-
+# Function for a valid Help for users
 def validHelp():
 	helpStr = """Hey There, Here are some list of commands for LeetCode
 
-	set [link] - For Setting up the workspace for the leet code question
-	    [link] - This denotes a valid link of the leetCode Problem. 
-	             Square Brackets are not neccesary.
+set [link] - For Setting up the workspace for the leet code question
+    [link] - This denotes a valid link of the leetCode Problem. 
+             Square Brackets are not neccesary.
 
-	check [file] - This command is used for checking the input and output for the 
-	               sample inputs provided by the LeetCode.
+check [file] - This command is used for checking the input and output for the 
+               sample inputs provided by the LeetCode.
 
-	visualize [Input Number] - This command is used to visualize any LeetCode Input test.
-							   (Under Process)
+visualize [Input Number] - This command is used to visualize any LeetCode Input test.
+						   (Under Process)
 
-	customVisualize [link] [file] - This command is used to visualize any Custom Input for 
-	                                the LeetCode question. (Under Process)
+customVisualize [link] [file] - This command is used to visualize any Custom Input for 
+                                the LeetCode question. (Under Process)
 	"""
 
 	print(helpStr)
 
-
+# Function if unknown common is found
 def invalidHelp():
 	print("Sorry Invalid command !\n\n")
 
